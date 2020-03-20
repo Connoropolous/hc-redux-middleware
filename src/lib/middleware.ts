799d26740b50e0066df18f68bfb677cbce59b29d
Connor Turland <connorturland@gmail.com>
2020-03-20 16:26:36 -0400

    dont dispatch twice and don't mod error

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -1,84 +1,91 @@
-import { Middleware, AnyAction } from 'redux'
+import { Middleware, AnyAction } from 'redux';
 
 type hcWebClientConnect = Promise<{
-  call: (callStr: string) => (params: any) => Promise<string>,
-  callZome: (instance: string, zome: string, func: string) => (params: any) => Promise<string>,
-  close: () => Promise<any>,
-  ws: any
-}>
+  call: (callStr: string) => (params: any) => Promise<string>;
+  callZome: (
+    instance: string,
+    zome: string,
+    func: string
+  ) => (params: any) => Promise<string>;
+  close: () => Promise<any>;
+  ws: any;
+}>;
 
-export const holochainMiddleware = (hcWc: hcWebClientConnect): Middleware => store => {
+export const holochainMiddleware = (
+  hcWc: hcWebClientConnect
+): Middleware => store => {
   // stuff here has the same life as the store!
   // this is how we persist a websocket connection
 
   const connectPromise = hcWc.then(({ call, callZome, ws }) => {
     ws.on('open', () => {
-      store.dispatch({ type: 'HOLOCHAIN_WEBSOCKET_CONNECTED' })
-    })
+      store.dispatch({ type: 'HOLOCHAIN_WEBSOCKET_CONNECTED' });
+    });
 
     ws.on('close', () => {
-      store.dispatch({ type: 'HOLOCHAIN_WEBSOCKET_DISCONNECTED' })
-    })
+      store.dispatch({ type: 'HOLOCHAIN_WEBSOCKET_DISCONNECTED' });
+    });
 
-    return { call, callZome }
-  })
+    return { call, callZome };
+  });
 
   return next => (action: AnyAction) => {
-    if (action.meta && (action.meta.holochainZomeCallAction || action.meta.holochainAdminAction)) {
-      next(action) // resend the original action so the UI can change based on requests
+    if (
+      action.meta &&
+      (action.meta.holochainZomeCallAction || action.meta.holochainAdminAction)
+    ) {
+      next(action); // resend the original action so the UI can change based on requests
 
       return connectPromise.then(({ call, callZome }) => {
-
-        let callFunction
+        let callFunction;
         if (action.meta.holochainZomeCallAction) {
-          const { instanceId, zome, func } = action.meta
-          callFunction = callZome(instanceId, zome, func)
+          const { instanceId, zome, func } = action.meta;
+          callFunction = callZome(instanceId, zome, func);
         } else {
-          callFunction = call(action.meta.callString)
+          callFunction = call(action.meta.callString);
         }
 
         return callFunction(action.payload)
           .then((rawResult: string) => {
-
             // holochain calls will strings (possibly stringified JSON)
             // while container admin calls will return parsed JSON
-            let result
+            let result;
             try {
-              result = JSON.parse(rawResult)
+              result = JSON.parse(rawResult);
             } catch (e) {
-              result = rawResult
+              result = rawResult;
             }
 
-            if (result.Err !== undefined) { // holochain error
-              store.dispatch({
-                type: action.type + '_FAILURE',
-                payload: result.Err
-              })
-              return Promise.reject(Error(result.Err))
-            } else if (result.Ok !== undefined) { // holochain Ok
+            if (result.Err !== undefined) {
+              // holochain error
+              return Promise.reject(result);
+            } else if (result.Ok !== undefined) {
+              // holochain Ok
               store.dispatch({
                 type: action.type + '_SUCCESS',
                 payload: result.Ok
-              })
-              return result.Ok
-            } else {                 // unknown. Return raw result as success
+              });
+              return result.Ok;
+            } else {
+              // unknown. Return raw result as success
               store.dispatch({
                 type: action.type + '_SUCCESS',
                 payload: result
-              })
-              return result
+              });
+              return result;
             }
           })
-          .catch((err: Error) => { // websocket error
+          .catch(err => {
+            // holochain or websocket error
             store.dispatch({
               type: action.type + '_FAILURE',
               payload: err
-            })
-            return Promise.reject(err)
-          })
-      })
+            });
+            return Promise.reject(err);
+          });
+      });
     } else {
-      return next(action)
+      return next(action);
     }
-  }
-}
+  };
+};
