a673536573f5360f10ce76789d065568be941f95
Connor Turland <connorturland@gmail.com>
2020-10-07 12:30:33 -0400

    up

--- src/lib/actionCreator.ts
+++ src/lib/actionCreator.ts
@@ -1,38 +1,87 @@
 import { AgentPubKey } from '@holochain/conductor-api';
 import { createAsyncAction } from 'typesafe-actions';
 
-export type DnaHash = {
-  hash: Buffer,
-  hash_type: Buffer,
+export type Hash = {
+  hash: Buffer;
+  hash_type: Buffer;
+};
+export type CellId = [Hash, AgentPubKey];
+
+const HASH_DIVIDER = '[:hash_divider:]';
+export function hashToString(hash: Hash) {
+  // nodejs
+  if (typeof window === 'undefined') {
+    return (
+      hash.hash_type.toString('hex') + HASH_DIVIDER + hash.hash.toString('hex')
+    );
+  }
+  // browser
+  else {
+    return hash.hash_type.toString() + HASH_DIVIDER + hash.hash.toString();
+  }
 }
-export type CellId = [DnaHash, AgentPubKey]
+
+export function hashFromString(str: string) {
+  const [hashtypestring, hashstring] = str.split(HASH_DIVIDER);
+  // nodejs
+  if (typeof window === 'undefined') {
+    return {
+      hash: Buffer.from(hashstring, 'hex'),
+      hash_type: Buffer.from(hashtypestring, 'hex')
+    };
+  }
+  // browser
+  else {
+    return {
+      hash: Buffer.from(hashstring.split(',')),
+      hash_type: Buffer.from(hashtypestring.split(','))
+    };
+  }
+}
+
+const CELL_ID_DIVIDER = '[:cell_id_divider:]';
+export function cellIdToString(cellId: CellId) {
+  // [DnaHash, AgentPubKey]
+  return hashToString(cellId[0]) + CELL_ID_DIVIDER + hashToString(cellId[1]);
+}
+
+export function cellIdFromString(str: string) {
+  // [DnaHash, AgentPubKey]
+  const [dnahashstring, agentpubkeyhashstring] = str.split(CELL_ID_DIVIDER);
+  return [hashFromString(dnahashstring), hashFromString(agentpubkeyhashstring)];
+}
+
+export interface HcPayload {
+  payload: any;
+  cellIdString: string;
+}
 
 /**
  *
  * Function that creates action creators for holochain zome function calls
  *
  */
-export const createZomeCallAsyncAction = <ParamType, ReturnType>(
-  cell_id: CellId,
+export const createZomeCallAsyncAction = (
   zome_name: string,
-  fn_name: string,
-  provenance: AgentPubKey
+  fn_name: string
 ) => {
-  const callString = [cell_id, zome_name, fn_name].join('/');
+  const callString = [zome_name, fn_name].join('/');
 
-  const action = createAsyncAction(
+  const asyncActionCreator = createAsyncAction(
     callString,
     callString + '_SUCCESS',
     callString + '_FAILURE'
-  )<ParamType, ReturnType, Error>();
+  );
+  const action = asyncActionCreator();
 
   const newAction = action as typeof action & {
-    create: (param: ParamType) => any;
-    sig: (param: ParamType) => Promise<ReturnType>;
+    create: (param: HcPayload) => any;
+    sig: (param: HcPayload) => Promise<any>;
   };
 
   // the action creators that are produced
-  newAction.create = (payload: ParamType) => {
+  newAction.create = (param: HcPayload) => {
+    const cell_id = cellIdFromString(param.cellIdString);
     return {
       type: callString,
       meta: {
@@ -40,9 +89,9 @@ export const createZomeCallAsyncAction = <ParamType, ReturnType>(
         cell_id,
         zome_name,
         fn_name,
-        provenance
+        provenance: cell_id[1] // AgentPubKey
       },
-      payload
+      payload: param.payload
     };
   };
 
--- src/lib/middleware.test.ts
+++ src/lib/middleware.test.ts
@@ -3,10 +3,13 @@ import sinon from 'sinon';
 
 import { holochainMiddleware } from './middleware';
 import {
-  CellId,
-  createZomeCallAsyncAction
+  // CellId,
+  createZomeCallAsyncAction,
+  cellIdToString,
+  HcPayload
   // createHolochainAdminAsyncAction
 } from './actionCreator';
+import { AppWebsocket } from '@holochain/conductor-api';
 
 const urlConfig = {
   appUrl: 'ws://localhost:8888',
@@ -25,149 +28,74 @@ const create = () => {
   return { store, next, invoke };
 };
 
-test('It passes non-holochain actions to the next reducer', async t => {
-  let { next, invoke } = create();
+(async () => {
+  const client = await AppWebsocket.connect('ws://localhost:8888');
+  const app_info = await client.appInfo({ app_id: 'test-app' });
+  const cell_id = app_info.cell_data[0][0];
 
-  const nonHolochainAction = { type: 'not-holochain-action' };
-  await invoke(nonHolochainAction);
+  test('It passes non-holochain actions to the next reducer', async t => {
+    let { next, invoke } = create();
 
-  t.true(next.calledWith(nonHolochainAction));
-});
+    const nonHolochainAction = { type: 'not-holochain-action' };
+    await invoke(nonHolochainAction);
 
-test('It passes holochain actions and dispatches new action on success', async t => {
-  let { next, invoke, store } = create();
+    t.true(next.calledWith(nonHolochainAction));
+  });
 
-  const profile = {
-    first_name: 'c',
-    last_name: 't',
-    handle: 'ct',
-    status: 'Online',
-    avatar_url: 'test',
-    address: '123123'
-  };
-  const actionCreator = createZomeCallAsyncAction(
-    cell_id,
-    'acorn_profiles',
-    'create_whoami',
-    cell_id[1]
-  );
-  const result = await invoke(actionCreator.create(profile));
+  test('It passes holochain actions and dispatches new action on success', async t => {
+    let { next, invoke, store } = create();
 
-  t.deepEqual(result.entry, profile);
-  t.true(next.calledWith(actionCreator.create(profile)));
-  t.true(store.dispatch.calledWith(actionCreator.success(result)));
-});
-
-test('It passes holochain actions and dispatches new error action on holochain error. Err is unwrapped ', async t => {
-  let { next, invoke, store } = create();
-
-  const actionCreator = createZomeCallAsyncAction(
-    cell_id,
-    'acorn_profiles',
-    'create_whoami',
-    cell_id[1]
-  );
-
-  try {
-    await invoke(actionCreator.create(null));
-  } catch (result) {
-    const e = new Error(
-      JSON.stringify({
-        type: 'Error',
-        data: {
-          type: 'RibosomeError',
-          data:
-            'Wasm error while working with Ribosome: Zome("failed to deserialize args: FromBytes(\\"invalid type: unit value, expected struct Profile\\")")'
-        }
-      })
+    const profile = {
+      first_name: 'c',
+      last_name: 't',
+      handle: 'ct',
+      status: 'Online',
+      avatar_url: 'test',
+      address: '123123'
+    };
+    const actionCreator = createZomeCallAsyncAction(
+      'acorn_profiles',
+      'create_whoami'
     );
-    t.deepEqual(result, e);
-    t.true(next.calledWith(actionCreator.create(null)));
-    t.deepEqual(store.dispatch.lastCall.args[0], actionCreator.failure(e));
-  }
-});
+    const param: HcPayload = {
+      cellIdString: cellIdToString(cell_id),
+      payload: profile
+    };
+    const result = await invoke(actionCreator.create(param));
 
-const cell_id: CellId = [
-  {
-    hash: Buffer.from([
-      0x54,
-      0x6c,
-      0x93,
-      0x85,
-      0x9e,
-      0x32,
-      0xa6,
-      0xa3,
-      0x1f,
-      0xb4,
-      0x0d,
-      0x4c,
-      0x75,
-      0x8d,
-      0x5e,
-      0xcd,
-      0x26,
-      0xdf,
-      0x72,
-      0xd3,
-      0xe2,
-      0xd8,
-      0x87,
-      0xde,
-      0x14,
-      0xe9,
-      0x79,
-      0x78,
-      0x49,
-      0x20,
-      0x95,
-      0x36,
-      0x64,
-      0x3f,
-      0x6b,
-      0xc2
-    ]),
-    hash_type: Buffer.from([0x84, 0x2d, 0x24])
-  },
-  {
-    hash: Buffer.from([
-      0x7c,
-      0xf2,
-      0x9d,
-      0xc1,
-      0xf4,
-      0xc6,
-      0x05,
-      0xe7,
-      0x7b,
-      0x88,
-      0x20,
-      0x8d,
-      0xce,
-      0x67,
-      0xbe,
-      0xd9,
-      0x37,
-      0x38,
-      0x94,
-      0x2c,
-      0x30,
-      0xe7,
-      0x0e,
-      0xcb,
-      0x2d,
-      0x60,
-      0x52,
-      0xea,
-      0x3e,
-      0x63,
-      0x2a,
-      0x5f,
-      0xb8,
-      0xbc,
-      0x66,
-      0xbd
-    ]),
-    hash_type: Buffer.from([0x84, 0x20, 0x24])
-  }
-];
+    t.deepEqual(result.entry, profile);
+    t.true(next.calledWith(actionCreator.create(param)));
+    t.true(store.dispatch.calledWith(actionCreator.success(result)));
+  });
+
+  test('It passes holochain actions and dispatches new error action on holochain error. Err is unwrapped ', async t => {
+    let { next, invoke, store } = create();
+
+    const actionCreator = createZomeCallAsyncAction(
+      'acorn_profiles',
+      'create_whoami'
+    );
+    const param: HcPayload = {
+      cellIdString: cellIdToString(cell_id),
+      payload: null
+    };
+
+    try {
+      await invoke(actionCreator.create(param));
+    } catch (result) {
+      const e = new Error(
+        JSON.stringify({
+          type: 'Error',
+          data: {
+            type: 'RibosomeError',
+            data:
+              'Wasm error while working with Ribosome: Zome("failed to deserialize args: FromBytes(\\"invalid type: unit value, expected struct Profile\\")")'
+          }
+        })
+      );
+      t.deepEqual(result, e);
+      t.true(next.calledWith(actionCreator.create(param)));
+      t.deepEqual(store.dispatch.lastCall.args[0], actionCreator.failure(e));
+    }
+  });
+})();
